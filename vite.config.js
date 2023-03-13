72d524a8a97929c01e35192b3ccc9697a7834f6c
AwesomeDevin <awesomedevin.deng@gmail.com>
2023-03-13 21:19:24 +0800

    chore: update vite config

--- vite.config.js
+++ vite.config.js
@@ -7,15 +7,24 @@ import commonjs from '@rollup/plugin-commonjs';
 export default  defineConfig({
   // 配置选项
   build: {
+    minify: false,
     lib: {
-      entry: [_path.resolve(__dirname, './src/webpack-plugin.ts')], // 设置入口文件
+      entry: [_path.resolve(__dirname, './src/webpack-plugin.ts'), _path.resolve(__dirname, './src/react/index.ts')], // 设置入口文件
       name: 'route-resource-preload', 
-      formats: ['es','cjs']
+      formats: ['es','cjs'],
+      fileName: (format, entryName) => {
+        if(entryName.match(/plugin/)){
+          return `${entryName}.js`
+        }else{
+          return `react/${entryName}.js`
+        }
+        
+      }
     },
     // sourcemap: true, // 输出.map文件
     rollupOptions: {
-      external: ['react','react-router-dom'],
+      external: ['react'],
     }
   },
-  plugins: [commonjs()]
+  plugins: [react(),commonjs()]
 })
