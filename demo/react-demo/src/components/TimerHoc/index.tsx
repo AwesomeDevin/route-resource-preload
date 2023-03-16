3bffc7a5bb2975ed6a6ef249ab0250f15cab207e
AwesomeDevin <awesomedevin.deng@gmail.com>
2023-03-16 11:44:56 +0800

    feat: optimise cache

---
+++ demo/react-demo/src/components/TimerHoc/index.tsx
@@ -0,0 +1,22 @@
+import  { useCallback, useRef } from "react";
+
+export default function Hoc(Com: any){
+  function  Timer(props: any){
+    const { onEnd } = props
+    const starTime = useRef({
+      second: new Date().getSeconds(),
+      milliseconds: new Date().getMilliseconds(),
+
+    })
+
+    const handleCallback = useCallback(() => {
+      const date = new Date()
+      const second = date.getSeconds() - starTime.current.second
+      const milliseconds = date.getMilliseconds() - starTime.current.milliseconds
+      onEnd && onEnd(second * 1000 + milliseconds)
+    }, [onEnd]);
+  
+    return <Com {...props} onEnd={handleCallback} />
+  }
+  return Timer
+}
