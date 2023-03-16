5fa05d3e110ca71f05dad0f89f94fe591af7a6a0
AwesomeDevin <awesomedevin.deng@gmail.com>
2023-03-16 12:56:16 +0800

    optimise demo

--- demo/react-demo/src/router/index.tsx
+++ demo/react-demo/src/router/index.tsx
@@ -37,19 +37,22 @@ export default function Router(){
   },[])
 
   return <>
-    <Switch>
-      <Route path='*' element={<>Index</>} />
-      <Route path='/A' element={<><TimerA onEnd={setVal} /></>} />
-      <Route path='/MF' element={<>
-        <TimerMF onEnd={setVal} width="300" src="https://img14.360buyimg.com/ling/s516x0_jfs/t1/97522/12/25179/1393762/622aa4c9E4ff1c9d2/3de6b0ab3c754b8d.png" />
-        </>} />
-    </Switch>
-    <div style={{marginTop: 20}}>Component Loading Time: {timestamp} (ms)</div>
-
-    <div className='tabs'>
+  <div className='tabs'>
       <a href='/'>Test Load</a>
       <a href='/?tab=preload'>Test preload</a>
     </div>    
+  
+  <div className='core'>
+    <Switch>
+      <Route path='*' element={<div style={{height: 250, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>This in Index</div>} />
+      <Route path='/A' element={<div style={{height: 250, display: 'flex', justifyContent: 'center', alignItems: 'center'}}><TimerA onEnd={setVal} /></div>} />
+      <Route path='/MF' element={<>
+        <TimerMF onEnd={setVal} height={250} src="https://img14.360buyimg.com/ling/s516x0_jfs/t1/97522/12/25179/1393762/622aa4c9E4ff1c9d2/3de6b0ab3c754b8d.png" />
+        </>} />
+    </Switch>
+    <div style={{marginTop: 20, color: '#ccc'}}>Component Loading Time: {timestamp} (ms)</div>
+
+    
         
     {!showPreload ? <div>
       <Link to="/A" >
@@ -67,5 +70,6 @@ export default function Router(){
         <Link to="/MF" >Preload MF</Link>
       </PreloadLink>
     </div>}
+  </div>
   </>
 }
